47fc0633844f98206c0d3e03c0546526199b43da
yanggengzhen <15017290625@163.com>
2022-09-04 17:09:24 +0800

    20220904-5.1-ts实现表单及jsonserver login 中间件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ProjectListScreen } from './screens/project-list/index';
+import { LoginScreen } from './screens/login/index';
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
+      <LoginScreen></LoginScreen>
+      {/* <ProjectListScreen></ProjectListScreen> */}
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,36 @@
+export const LoginScreen = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const login = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (response) => {
+      if (response.ok) {
+      }
+    });
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={"username"} />
+      </div>
+      <div>
+        <label htmlFor="username">密码</label>
+        <input type="text" id={"password"} />
+      </div>
+      <button type={"submit"}>登录</button>
+    </form>
+  );
+};
