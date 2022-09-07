826d19601cffcddf623b4b399c53b2a846cac19d
yanggengzhen <15017290625@163.com>
2022-09-08 00:19:17 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { ProjectListScreen } from "./screens/project-list/index";
 import { useAuth } from "./context/auth-context";
 import { AuthenticatedApp } from "authenticated-app";
 import { UnauthenticatedApp } from "unauthenticated-app";
 
 function App() {
   const { user } = useAuth();
-  console.log(user);
   return (
     <div className="App">
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
