7bbadef6c31d648280514421032e9267ce8dac6f
yanggengzhen <15017290625@163.com>
2022-09-14 22:47:13 +0800

    8.6-0914-用useMemo解决依赖循环问题

--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -1,14 +1,20 @@
+import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 // 返回页面url中，指定键的参数值（拿到url上的参数，赋值给input）
-export const useUrlQueryParam = (keys: string[]) => {
+export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   // setSearchParams用来改变url query的参数值
   const [searchParams, setSearchParams] = useSearchParams();
   // 返回两个参数
   return [
-    keys.reduce((prev, key) => {
-      return { ...prev, [key]: searchParams.get(key) || "" };
-    }, {} as { [key in string]: string }),
+    // 使用useMemo 对第一个引用类型进行依赖项，只有searchParams更新才会重新触发到引用类型改变，从而避免死循环
+    useMemo(
+      () =>
+        keys.reduce((prev, key) => {
+          return { ...prev, [key]: searchParams.get(key) || "" };
+        }, {} as { [key in K]: string }),
+      [searchParams]
+    ),
     setSearchParams,
   ] as const;
 };
